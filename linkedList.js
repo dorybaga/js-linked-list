68cb6e84790f2bc46148ac26daf6c62e7b8cdfe0
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-24 20:10:45 -1000

    module obj has methods available thru linked list obj

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,38 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  function getHead() {
 
+  }
+
+  function getTail() {
+
+  }
+
+  function add() {
+
+  }
+
+  function get() {
+
+  }
+
+  function remove() {
+
+  }
+
+  function insert() {
+
+  }
+
+  return {
+
+    getHead : getHead,
+    getTail : getTail,
+    add : add,
+    get : get,
+    remove : remove,
+    insert : insert
+
+  };
 }
